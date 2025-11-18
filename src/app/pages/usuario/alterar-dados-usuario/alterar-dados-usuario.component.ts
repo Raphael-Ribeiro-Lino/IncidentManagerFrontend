7e5d780b6d46dc6c4a31e10e6be67a8c0001ad2e
Raphael Ribeiro Lino <raphar.lino@gmail.com>
2025-11-17 23:25:18 -0300

    Melhorando tela alterar dados usuario

--- src/app/pages/usuario/alterar-dados-usuario/alterar-dados-usuario.component.ts
+++ src/app/pages/usuario/alterar-dados-usuario/alterar-dados-usuario.component.ts
@@ -14,39 +14,34 @@ import { AuthService } from '../../../services/auth/auth.service';
 import { EmpresaOutput } from '../../../models/empresa/empresaOutput';
 import { UsuarioTokenOutput } from '../../../models/usuario/usuarioTokenOutput';
 import { UsuarioOutput } from '../../../models/usuario/usuarioOutput';
-import { NgIf, NgFor } from '@angular/common';
 import { UsuarioInput } from '../../../models/usuario/usuarioInput';
 
 @Component({
   selector: 'app-alterar-dados-usuario',
   standalone: true,
-  imports: [CommonModule, ReactiveFormsModule, RouterModule, NgIf, NgFor],
+  imports: [CommonModule, ReactiveFormsModule, RouterModule],
   templateUrl: './alterar-dados-usuario.component.html',
   styleUrls: ['./alterar-dados-usuario.component.css'],
 })
 export class AlterarDadosUsuarioComponent implements OnInit {
-  perfilLabels: Record<PerfilEnum, string> = {
+  formUsuario!: FormGroup;
+  errorMessages: string[] = [];
+  successfullyUpdatedUsuario = '';
+
+  token = localStorage.getItem('token')!;
+  usuarioLogado!: UsuarioTokenOutput;
+  usuarioId!: number;
+
+  empresaSelecionada: EmpresaOutput | null = null;
+
+  perfilLabels = {
     [PerfilEnum.ADMIN_EMPRESA]: 'Administrador da Empresa',
     [PerfilEnum.USUARIO]: 'Usuário Comum',
     [PerfilEnum.TECNICO_TI]: 'Técnico de TI',
   };
 
-  formUsuario!: FormGroup;
-  errorMessages: string[] = [];
-  successfullyUpdatedUsuario = '';
-  token = localStorage.getItem('token') as string;
-  perfis: PerfilEnum[] = Object.values(PerfilEnum);
-
-  empresaSelecionada: EmpresaOutput | null = null;
-  empresasFiltradas: EmpresaOutput[] = [];
-  mostrarDropdown = false;
-  ultimaBusca = '';
-  mensagemNenhumaEmpresa = '';
-  usuarioLogado!: UsuarioTokenOutput;
-  usuarioId!: number;
-
   constructor(
-    private formBuilder: FormBuilder,
+    private fb: FormBuilder,
     private usuarioService: UsuarioService,
     private empresaService: EmpresaService,
     private authService: AuthService,
@@ -57,104 +52,54 @@ export class AlterarDadosUsuarioComponent implements OnInit {
   ngOnInit(): void {
     this.usuarioLogado = this.authService.getUsuarioLogado()!;
     this.usuarioId = Number(this.route.snapshot.paramMap.get('id'));
+
     if (!this.usuarioId) {
-      this.errorMessages.push('ID do usuário inválido');
+      this.errorMessages.push('ID do usuário inválido.');
       return;
     }
 
     this.usuarioService.buscarPorId(this.token, this.usuarioId).subscribe({
-      next: (usuario: UsuarioOutput) => {
-        this.initForm(usuario);
-      },
-      error: () => {
-        this.errorMessages.push('Erro ao carregar dados do usuário.');
-      },
+      next: (usuario) => this.initForm(usuario),
+      error: () =>
+        this.errorMessages.push('Erro ao carregar dados do usuário.'),
     });
   }
 
   private initForm(usuario: UsuarioOutput) {
-    this.empresaSelecionada = usuario.empresa || null;
+    this.empresaSelecionada = usuario.empresa ?? null;
 
-    const formConfig: any = {
+    this.formUsuario = this.fb.group({
       nome: [
-        usuario.nome || '',
-        [Validators.required, Validators.maxLength(100)],
+        usuario.nome,
+        [
+          Validators.required,
+          Validators.maxLength(100),
+          Validators.pattern(/^[A-Za-zÀ-ÖØ-öø-ÿ]+(?:[ -][A-Za-zÀ-ÖØ-öø-ÿ]+)*$/),
+        ],
       ],
       email: [
-        usuario.email || '',
-        [Validators.required, Validators.maxLength(320)],
+        usuario.email,
+        [
+          Validators.required,
+          Validators.maxLength(320),
+          Validators.pattern(
+            /^(?=.{1,64}@)[A-Za-z0-9_-]+(\.[A-Za-z0-9_-]+)*@[^-][A-Za-z0-9-]+(\.[A-Za-z0-9-]+)*(\.[A-Za-z]{2,})$/
+          ),
+        ],
       ],
       telefone: [
-        usuario.telefone || '',
+        usuario.telefone,
         [
           Validators.required,
           Validators.minLength(10),
           Validators.maxLength(15),
+          Validators.pattern(/^\+?\d{0,3}?\s?\(?\d{2,3}\)?\s?\d{4,5}-?\d{4}$/),
         ],
       ],
       ativo: [usuario.ativo, Validators.required],
       perfil: [usuario.perfil, Validators.required],
-      empresa: [usuario.empresa?.id || '', Validators.required],
-    };
-
-    this.formUsuario = this.formBuilder.group(formConfig);
-  }
-
-  filtrarEmpresas(event: Event) {
-    if (this.usuarioLogado.perfil === 'ADMIN_EMPRESA') return;
-    const valor = (event.target as HTMLInputElement).value.trim();
-    if (!valor) {
-      this.ultimaBusca = '';
-      this.buscarEmpresas();
-      this.mostrarDropdown = true;
-      return;
-    }
-    if (valor === this.ultimaBusca) return;
-    this.ultimaBusca = valor;
-    this.empresaService.listar(this.token, 0, valor).subscribe({
-      next: (data) => {
-        this.empresasFiltradas = data.content;
-        this.mostrarDropdown = true;
-        this.mensagemNenhumaEmpresa =
-          this.empresasFiltradas.length === 0
-            ? 'Nenhuma empresa encontrada.'
-            : '';
-      },
-      error: () => {
-        this.empresasFiltradas = [];
-        this.mensagemNenhumaEmpresa = 'Nenhuma empresa encontrada.';
-      },
+      empresa: [usuario.empresa?.id ?? '', Validators.required],
     });
-  }
-
-  buscarEmpresas(search: string = '', page: number = 0) {
-    this.empresaService.listar(this.token, page, search).subscribe({
-      next: (data) => {
-        this.empresasFiltradas =
-          page === 0
-            ? data.content
-            : [...this.empresasFiltradas, ...data.content];
-        this.mensagemNenhumaEmpresa =
-          this.empresasFiltradas.length === 0
-            ? 'Nenhuma empresa encontrada.'
-            : '';
-      },
-      error: () => {
-        this.empresasFiltradas = [];
-        this.mensagemNenhumaEmpresa = 'Nenhuma empresa encontrada.';
-      },
-    });
-  }
-
-  selecionarEmpresa(empresa: EmpresaOutput) {
-    this.empresaSelecionada = empresa;
-    this.formUsuario.get('empresa')?.setValue(empresa.id);
-    this.formUsuario.get('empresa')?.markAsTouched();
-    this.mostrarDropdown = false;
-  }
-
-  fecharDropdown() {
-    setTimeout(() => (this.mostrarDropdown = false), 150);
   }
 
   cancelar() {
@@ -162,8 +107,25 @@ export class AlterarDadosUsuarioComponent implements OnInit {
     window.scrollTo({ top: 0, behavior: 'smooth' });
   }
 
+  private formatarTelefone(telefone: string): string {
+    const numeros = telefone.replace(/\D/g, '');
+
+    if (numeros.length === 11)
+      return `(${numeros.slice(0, 2)}) ${numeros.slice(2, 7)}-${numeros.slice(
+        7
+      )}`;
+
+    if (numeros.length === 10)
+      return `(${numeros.slice(0, 2)}) ${numeros.slice(2, 6)}-${numeros.slice(
+        6
+      )}`;
+
+    return numeros;
+  }
+
   submitForm() {
     this.errorMessages = [];
+
     if (this.formUsuario.invalid) {
       this.errorMessages.push(
         'Corrija os erros do formulário antes de salvar.'
@@ -172,33 +134,18 @@ export class AlterarDadosUsuarioComponent implements OnInit {
       return;
     }
 
-    const payload: UsuarioInput = { ...this.formUsuario.value };
-
-    if (payload.nome) {
-      payload.nome = payload.nome
-        .trim()
+    let payload: UsuarioInput = {
+      ...this.formUsuario.value,
+      nome: this.formUsuario.value.nome
+        ?.trim()
         .replace(/\s+/g, ' ')
         .split(' ')
         .map(
           (p: string) => p.charAt(0).toUpperCase() + p.slice(1).toLowerCase()
         )
-        .join(' ');
-    }
-
-    if (payload.telefone) {
-      const numeros = payload.telefone.replace(/\D/g, '');
-      if (numeros.length === 11)
-        payload.telefone = `(${numeros.slice(0, 2)}) ${numeros.slice(
-          2,
-          7
-        )}-${numeros.slice(7)}`;
-      else if (numeros.length === 10)
-        payload.telefone = `(${numeros.slice(0, 2)}) ${numeros.slice(
-          2,
-          6
-        )}-${numeros.slice(6)}`;
-      else payload.telefone = numeros;
-    }
+        .join(' '),
+      telefone: this.formatarTelefone(this.formUsuario.value.telefone),
+    };
 
     this.usuarioService
       .alterarDados(this.token, this.usuarioId, payload)
