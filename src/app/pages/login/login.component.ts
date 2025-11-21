308c412fa017fc4264d7eb0b05024c7adba570a7
Raphael Ribeiro Lino <raphar.lino@gmail.com>
2025-11-21 03:49:01 -0300

    Melhorando design tela de login

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,12 +1,19 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 import { Router, RouterModule } from '@angular/router';
 import { MatIconModule } from '@angular/material/icon';
 import { LoginInput } from '../../models/login/loginInput';
 import { LoginService } from '../../services/login/login.service';
 import { LoginOutput } from '../../models/login/loginOutput';
-
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-login',
@@ -15,10 +22,13 @@ import { LoginOutput } from '../../models/login/loginOutput';
     CommonModule,
     ReactiveFormsModule,
     RouterModule,
-    MatIconModule
+    MatIconModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatButtonModule,
   ],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrl: './login.component.css',
 })
 export class LoginComponent implements OnInit {
   formLogin!: FormGroup;
@@ -31,23 +41,45 @@ export class LoginComponent implements OnInit {
     this.showPassword = !this.showPassword;
   }
 
-
-  constructor(private formBuilder: FormBuilder, private loginService: LoginService, private route: Router) {
+  constructor(
+    private formBuilder: FormBuilder,
+    private loginService: LoginService,
+    private route: Router
+  ) {
     this.formLogin = this.formBuilder.group({
-      email: ['', [Validators.required, Validators.maxLength(320), Validators.pattern("^(?=.{1,64}@)[A-Za-z0-9_-]+(\\.[A-Za-z0-9_-]+)*@[^-][A-Za-z0-9-]+(\\.[A-Za-z0-9-]+)*(\\.[A-Za-z]{2,})$")]],
-      password: ['', [Validators.required, Validators.minLength(8), Validators.maxLength(255), Validators.pattern("^(?=.*[A-Za-z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$")]]
+      email: [
+        '',
+        [
+          Validators.required,
+          Validators.maxLength(320),
+          Validators.pattern(
+            '^(?=.{1,64}@)[A-Za-z0-9_-]+(\\.[A-Za-z0-9_-]+)*@[^-][A-Za-z0-9-]+(\\.[A-Za-z0-9-]+)*(\\.[A-Za-z]{2,})$'
+          ),
+        ],
+      ],
+      password: [
+        '',
+        [
+          Validators.required,
+          Validators.minLength(8),
+          Validators.maxLength(255),
+          Validators.pattern(
+            '^(?=.*[A-Za-z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$'
+          ),
+        ],
+      ],
     });
     const currentNavigation = route.getCurrentNavigation();
     if (currentNavigation?.extras?.state?.['successData']) {
-      this.successfullyRegisteredUser = currentNavigation?.extras?.state?.['successData'];
+      this.successfullyRegisteredUser =
+        currentNavigation?.extras?.state?.['successData'];
       setTimeout(() => {
-        this.successfullyRegisteredUser = "";
+        this.successfullyRegisteredUser = '';
       }, 3000);
     }
   }
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
   submitForm(): void {
     this.errorMessages = [];
@@ -59,22 +91,23 @@ export class LoginComponent implements OnInit {
 
     const loginInput: LoginInput = {
       email: this.formLogin.value.email,
-      senha: this.formLogin.value.password
+      senha: this.formLogin.value.password,
     };
 
     this.loginService.auth(loginInput).subscribe({
       next: (data: LoginOutput) => {
         localStorage.setItem('token', data.token);
-        this.route.navigate(["home"]);
+        this.route.navigate(['home']);
       },
       error: (error) => {
         if (error.error && error.error.message) {
           this.errorMessages.push(error.error.message);
         } else {
-          this.errorMessages.push('Ocorreu um erro inesperado. Tente mais tarde, por favor!');
+          this.errorMessages.push(
+            'Ocorreu um erro inesperado. Tente mais tarde, por favor!'
+          );
         }
-      }
-    }
-    );
+      },
+    });
   }
 }
