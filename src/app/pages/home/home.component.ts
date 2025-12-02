42b19d7661b142e039ba66e9ecdc441fac3536d3
Raphael Ribeiro Lino <raphar.lino@gmail.com>
2025-12-02 15:42:41 -0300

    adicionando tela home

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,12 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { MatIconModule } from '@angular/material/icon';
+import { MatCardModule } from '@angular/material/card';
+
+import { AuthService } from '../../services/auth/auth.service';
+import { HeaderPerfilEnum } from '../../models/usuario/headerPerfilEnum';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, RouterModule, MatIconModule, MatCardModule],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrl: './home.component.css',
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  nomeUsuario: string = '';
+  perfil: string = '';
 
+  // Disponibiliza o Enum para o HTML
+  PerfilEnum = HeaderPerfilEnum;
+
+  constructor(private authService: AuthService) {}
+
+  ngOnInit(): void {
+    const user = this.authService.getUsuarioLogado();
+    if (user) {
+      this.nomeUsuario = user.nome.split(' ')[0]; // Primeiro nome
+      this.perfil = user.perfil;
+    }
+  }
 }
